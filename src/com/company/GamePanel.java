88d8e392633cd8c3cbda241935c333b18c288c59
zzzhanaidarova <212285@astanait.edu.kz>
2022-02-16 15:01:59 +0600

    Taking information from buttons like up, down, right, and left and making our player move

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import  javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     // screen setting
     final int originalTitleSize = 16;
     final int scale = 3;
@@ -14,17 +14,78 @@ public class GamePanel extends JPanel {
     final int screenWidth = titleSize * maxScreenCol;
     final int screenHeight = titleSize * maxScreenRow;
 
+    //Frame per second
+    int fps = 60;
+
+    KeyHandler key = new KeyHandler();
+    Thread gameThread;
+
+    //setting player's first position
+    int playerX = screenWidth/2;
+    int playerY = screenHeight/2;
+    int playerSpeed = 5;
+
     public GamePanel () {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.pink);
         this.setDoubleBuffered(true);
+        this.addKeyListener(key);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double interval = 1000000000/fps; //1 second = 10^9 nanoseconds
+        double nextTime = System.nanoTime() + interval;
+        while (gameThread != null)
+        {
+            update();
+            repaint();
+
+            try {
+                double left = nextTime - System.nanoTime();
+                left = left/1000000;
+                if (left < 0)
+                {
+                    left = 0;
+                }
+                Thread.sleep((long) left);
+                nextTime += interval;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update() {
+        if(key.bottomUp)
+        {
+            playerY -= playerSpeed;
+        }
+        else if (key.bottomDown)
+        {
+            playerY += playerSpeed;
+        }
+        else if (key.bottomLeft)
+        {
+            playerX -= playerSpeed;
+        }
+        else if (key.bottomRight)
+        {
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.setColor(Color.getHSBColor(160, 200, 230));
-        g2.fillRect(100, 100, titleSize, titleSize);
+        g2.fillRect(playerX, playerY, titleSize, titleSize);
         g2.dispose();
     }
 }
--- src/com/company/KeyHandler.java
+++ src/com/company/KeyHandler.java
@@ -4,6 +4,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+    public boolean bottomUp, bottomDown, bottomLeft, bottomRight;
+
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -11,11 +13,43 @@ public class KeyHandler implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_UP)
+        {
+            bottomUp = true;
+        }
+        if (code == KeyEvent.VK_DOWN)
+        {
+            bottomDown = true;
+        }
+        if (code == KeyEvent.VK_RIGHT)
+        {
+            bottomRight = true;
+        }
+        if(code == KeyEvent.VK_LEFT)
+        {
+            bottomLeft = true;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_UP)
+        {
+            bottomUp = false;
+        }
+        if (code == KeyEvent.VK_DOWN)
+        {
+            bottomDown = false;
+        }
+        if (code == KeyEvent.VK_RIGHT)
+        {
+            bottomRight = false;
+        }
+        if(code == KeyEvent.VK_LEFT)
+        {
+            bottomLeft = false;
+        }
     }
 }
