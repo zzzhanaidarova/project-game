6dd3aa832da71cddf873736157ad89e925ada4d6
itzhnrlm <96815154+itzhnrlm@users.noreply.github.com>
2022-02-25 01:30:14 +0600

    Create OBJ_Bone.java

---
+++ src/object/OBJ_Bone.java
@@ -0,0 +1,12 @@
+package object;
+
+import java.awt.image.BufferedImage;
+
+public class OBJ_Bone extends SuperObject{
+
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int worldX, worldY;
+
+}
